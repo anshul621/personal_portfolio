45e77fdb3a4cbb48b2393f34e012ca0c6857ee66
Anshul Shrivastava <as912@snu.edu.in>
2023-10-10 15:59:46 +0530

    Project Portfolio

---
+++ contacts.js
@@ -0,0 +1,7 @@
+// Delay the page load by 1000 milliseconds (1 second)
+setTimeout(function () {
+    // Get the page wrapper element
+    var pageWrapper = document.getElementById("page-wrapper");
+    // Set the opacity to 1 to reveal the content
+    pageWrapper.style.opacity = "1";
+}, 1000);
